586ac592c03c0e0283eb0ca4ca42267f0f4a9090
Eric Planas <noga.epc@gmail.com>
2017-02-13 16:10:13 +0100

    Print Hello GitHub

--- HelloGitHub/HelloGitHub.cpp
+++ HelloGitHub/HelloGitHub.cpp
@@ -1,11 +1,10 @@
-// HelloGitHub.cpp : Defines the entry point for the console application.
-//
-
+#include <iostream>
 #include "stdafx.h"
+using namespace std;
 
 
-int main()
+void main()
 {
-    return 0;
+	cout << "Hello GitHub!";
 }
 
